25df2a5683ad947a69ebbf309834c398ad1b796a
A.N. Protasevich <larsplotner@gmail.com>
2023-03-19 21:45:51 +0300

    feat:3.11 add frontend part

--- index.js
+++ index.js
@@ -9,9 +9,11 @@ morgan.token("content", (request, response)=>{
   }
 })
 
+
 app.use(cors());
 app.use(express.json());
 app.use(morgan(":method :url :status :res[content-length] - :response-time ms :content"));
+app.use(express.static("build"));
 
 let persons = [
   { 
