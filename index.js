ed6c7250217d58616e0c6aabae871f4eeea29bce
A.N. Protasevich <larsplotner@gmail.com>
2023-03-26 21:02:58 +0300

    feat:3.15-3.18

--- index.js
+++ index.js
@@ -4,7 +4,8 @@ const morgan = require("morgan");
 const cors = require("cors");
 const app = express();
 
-const Person = require("./models/person")
+const Person = require("./models/person");
+
 
 morgan.token("content", (request, response)=>{
   if (request.method === "POST") {
@@ -12,10 +13,10 @@ morgan.token("content", (request, response)=>{
   }
 })
 
+app.use(express.static("build"));
 app.use(cors());
 app.use(express.json());
 app.use(morgan(":method :url :status :res[content-length] - :response-time ms :content"));
-app.use(express.static("build"));
 
 
 app.get("/info", (request, response)=>{
@@ -56,26 +57,49 @@ app.post("/api/persons", (request, response)=>{
   })
 })
 
-app.get("/api/persons/:id", (request, response)=>{
-  const id = Number(request.params.id);
-  const person = persons.find(p=>p.id===id);
-
-  if (person){
-    return response.json(person)
-  } else {
-    response.status(404).end()
-  }
+app.get("/api/persons/:id", (request, response, next)=>{
+  Person.findById(request.params.id)
+    .then(person => {
+      if (person) {
+        response.json(person);
+      } else {
+        response.status(404).end();
+      }
+    })
+    .catch(error => {next(error)})
 });
 
-app.delete("/api/persons/:id", (request, response)=>{
-  const id = Number(request.params.id);
-  if (persons.find(p=>p.id===id)){
-    persons = persons.filter(p=>p.id !== id)
-    response.status(204).end()
-  } else {
-    response.status(404).end()
-  }
+app.delete("/api/persons/:id", (request, response, next)=>{
+  Person.findByIdAndRemove(request.params.id)
+    .then(result=>{
+      response.status(204).end();
+    })
+    .catch(error=>{next(error)})
 });
+
+app.put("/api/persons/:id", (request, response, next)=>{
+  const body = request.body
+
+  const person = {
+    name: body.name,
+    number: body.number
+  }
+
+  Person.findByIdAndUpdate(request.params.id, person, {new:true})
+    .then(updatedPerson=>response.json(updatedPerson))
+    .catch(error=>next(error))
+})
+
+const errorHandler = (error, request, response, next) => {  
+  console.log(error.message);
+
+  if (error.name === "CastError") {
+    return response.status(400).send({error:"malformatted id"})
+  }
+
+  next(error)
+}
+app.use(errorHandler)
 
 const PORT = process.env.PORT;
 app.listen(PORT, () => {
